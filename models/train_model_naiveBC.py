4e7454546292dc4a2bdf097f50adeb7922094b33
peteris racinskis <peterisracinskis@gmail.com>
2022-03-30 17:33:43 +0300

    simple neural net; need to see how it works tomorrow

--- models/train_model_naiveBC.py
+++ models/train_model_naiveBC.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python3
 import pandas as pd
 import numpy as np
 from os import listdir
 from typing import Tuple
+import tensorflow as tf
+from tensorflow.keras import layers, optimizers, losses, activations, regularizers, models
 TRAIN="processed_data/train_datasets/train-003430811ff20c35ccd5.csv"
 TEST="processed_data/train_datasets/test-003430811ff20c35ccd5.csv"
 
@@ -10,6 +13,35 @@ def data_and_label(df: pd.DataFrame) -> Tuple[np.ndarray]:
     label_cols = df.columns[-8:]
     return df[data_cols].values, df[label_cols].values
 
-train_data, train_labels = data_and_label(pd.read_csv(TRAIN))
-test_data, test_labels = data_and_label(pd.read_csv(TEST))
-pass
+def generate_trajectory(model, initial_state):
+    pass
+
+
+if __name__ == "__main__":
+    train_data, train_labels = data_and_label(pd.read_csv(TRAIN))
+    test_data, test_labels = data_and_label(pd.read_csv(TEST))
+   
+    model = tf.keras.Sequential([
+        layers.Dense(512),
+        layers.ReLU(),
+        layers.Dense(512),
+        layers.ReLU(),
+        layers.Dense(train_labels[0].size, activation=None),
+    ])
+
+    model.compile(
+        optimizer=optimizers.Adam(learning_rate=0.0001),
+        loss=losses.Huber()
+    )
+
+    history = model.fit(
+        train_data,
+        train_labels,
+        batch_size=4096,
+        epochs=20,
+        validation_data=(test_data, test_labels),
+        validation_freq=1,
+        verbose=2,
+    )
+
+    pass
