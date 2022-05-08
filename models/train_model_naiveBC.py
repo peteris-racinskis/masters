80d60e9f52f3ddd688924b602515884d73bed7ac
peteris racinskis <peterisracinskis@gmail.com>
2022-05-08 11:57:54 +0300

    add validation code to naive bc

--- models/train_model_naiveBC.py
+++ models/train_model_naiveBC.py
@@ -13,12 +13,15 @@ TRAIN="processed_data/train_datasets/train-start-time-5e9156387f59cb9efb35.csv"
 TEST="processed_data/train_datasets/test-start-time-5e9156387f59cb9efb35.csv"
 TEST="processed_data/train_datasets/test-start-time-doubled-7db3d40f19abc9f24f46-prep.csv"
 TRAIN="processed_data/train_datasets/train-start-time-doubled-7db3d40f19abc9f24f46-prep.csv"
-H_NEURONS=128
-EPOCHS=100
-OFILE=f"models/naiveBCx{H_NEURONS}x2-newdata-fixed-data-fastrelease-ep{EPOCHS}-norm-start-timesignal-partloss"
+H_NEURONS=256
+EPOCHS=50
+OFILE=f"models/naiveBCx{H_NEURONS}x2-newdata-fixed-data-fastrelease-ep{EPOCHS}-norm-start-timesignal"
 OVERWRITE=False
 STARTINDEX=0
 ID=""
+TIME="Time"
+REPEATS_IN_DATASET=1
+PREPEND=True
 huber = losses.Huber()
 entropy = losses.BinaryCrossentropy(from_logits=True)
 
@@ -64,6 +67,24 @@ def quaternion_norm(df: pd.DataFrame):
     quat_norm_series = df[quat_cols].pow(2).sum(axis=1).pow(1/2)
     quat_norm_series.name = "quaternion_norm"
     return pd.concat([df,quat_norm_series], axis=1), True
+
+def validation_on_test(df: pd.DataFrame, model, prep=PREPEND):
+    stop_offset = -1 * REPEATS_IN_DATASET if prep else 0
+    start_indices = list(df.loc[lambda d: d[TIME] == 0].index)
+    start_indices += [df.index[-1] - stop_offset + 2]
+    demos_with_trajectories = []
+    columns = list(df.columns[:12])
+    columns += [c for c in "xyz"]
+    columns += ["r"+c for c in "xyzw"]
+    columns += ["Released-model"]
+    for i, ii in zip(start_indices[:-1], start_indices[1:]):
+        demo, _ = data_and_label(df.iloc[i:ii+stop_offset], shuffle=False)
+        initial_state = demo[0:1]
+        trajectory = generate_trajectory(model, initial_state, len(demo))
+        demos_with_trajectories.append(np.concatenate([demo, trajectory[:,1:-3]], axis=1))
+    combined_data = np.concatenate(demos_with_trajectories, axis=0)
+    combined_df = pd.DataFrame(data=combined_data, columns=columns)
+    return combined_df
 
 if __name__ == "__main__":
     np.random.seed(133)
@@ -119,6 +140,7 @@ if __name__ == "__main__":
     #start = pd.read_csv(TRAIN).values[STARTINDEX,:-8]
     #trajectory = generate_trajectory(model, start, 100)
     trajectory = generate_trajectories_with_target(model, target_means, target_sds)
+    validated_dataset = validation_on_test(pd.read_csv(TEST), model)
     #trajectory = generate_trajectories_with_start(model, start_means, start_sds)
     cols = ["Time","x","y","z","rx", "ry", "rz", "rw", "Released", "xt", "yt", "zt"]
     df = pd.DataFrame(data=trajectory, columns=cols)
@@ -126,4 +148,4 @@ if __name__ == "__main__":
     #t = pd.Series(data=np.arange(0,5,0.01), name="Time")
     #output = pd.concat([t,df], axis=1)
     df.to_csv(OFILE+f"-{STARTINDEX}-{ID}.csv", index=False)
-    pass
+    validated_dataset.to_csv(OFILE.replace("models/", "models/validation/")+".csv", index=False)
